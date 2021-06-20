2947af2b851e1741e3bb0742044e9b26793019b9
Sachin <sachin.mehta0397@gmail.com>
2021-06-20 16:38:09 +0530

    adding final header file

---
+++ src/common/Constants.js
@@ -0,0 +1,21 @@
+export const LOGIN_CREDENTIALS_ERROR_MSG = "Invalid credentials!";
+
+/* Enum for HTTP Methods */
+export const ApiRequestTypeEnum = Object.freeze({
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE",
+});
+
+/* Enum for the Display of any Element */
+export const DisplayClassname = Object.freeze({
+  DISPLAY_NONE: "dispNone",
+  DISPLAY_BLOCK: "dispBlock",
+});
+
+/* Enum for the Type of Value */
+export const ValueTypeEnum = Object.freeze({
+  FORM_FIELD: 1,
+  VALIDATION_MESSAGE: 2,
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';  
+import Home from './screens/home/Home';
+import Details from './screens/details/Details';
+import Checkout from './screens/checkout/Checkout';
+import Profile from './screens/profile/Profile';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+class FoodApp extends Component { 
+    constructor(){
+        super()
+        this.baseUrl = "http://localhost:8080/api/" //setting the baseUrl of the api
+    }
+
+   render(){
+        return(
+            <Router>
+                <div className = 'main-conatiner'>
+                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/> {/* Route to home Page */ }
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} /> {/* Route to restaurant details Page */}
+                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} /> {/* Route to Profile Page */}
+                    <Route path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} /> {/* Route to Checkout Page */}
+                </div>
+            </Router> 
+        )
+    }
+}
+ 
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
+    <FoodApp/>,
     document.getElementById('root')
 );
 
